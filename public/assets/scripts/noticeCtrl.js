13ae5204280c3c0f612876388d17b49f34c49795
geof <geovandimaano.0728@gmail.com>
2017-07-28 20:36:20 +0800

    validation srv_resp

--- public/assets/scripts/noticeCtrl.js
+++ public/assets/scripts/noticeCtrl.js
@@ -2,46 +2,40 @@ angular.module("vavaGaming").controller('noticeCtrl', function($scope, $http) {
     
     $http.get("notice/api/get-all-sites")
         .then(function success(srv_resp){
-            $scope.sites    = srv_resp.data[0];
-            $scope.events   = angular.copy($scope.sites);
-            $scope.pag_inf  = srv_resp.data[1];
+            $scope.master   = srv_resp.data;
+            $scope.events   = angular.copy($scope.master.sites);
         }, function failed(srv_resp) {
-            $scope.sites    = [{}];
+            $scope.master    = [{}];
         }
     );
     
     $scope.searchNotice = function() {
         $http.post("notice/api/search-notice", $scope.noticesearch)
             .then(function success(srv_resp){
-                $scope.sites    = srv_resp.data[0];
-                $scope.events   = angular.copy($scope.sites);
-                $scope.pag_inf  = srv_resp.data[1];
-            }, function failed(srv_resp) {
-                $scope.sites    = [{}];
-            }
+            $scope.master   = srv_resp.data;
+        }, function failed(srv_resp) {
+            $scope.master    = [{}];
+        }
         );
     }
     $scope.searchEvent = function() {
         $http.post("notice/api/search-event", $scope.eventsearch)
             .then(function success(srv_resp){
-                $scope.sites    = srv_resp.data[0];
-                $scope.events   = angular.copy($scope.sites);
-                $scope.pag_inf  = srv_resp.data[1];
-            }, function failed(srv_resp) {
-                $scope.sites    = [{}];
-            }
+            $scope.master   = srv_resp.data;
+        }, function failed(srv_resp) {
+            $scope.master    = [{}];
+        }
         );
     }
 
     $scope.deleteNotice =function(){
         
-        $http.post("notice/api/delete-notices", $scope.sites)
+        $http.post("notice/api/delete-notices", $scope.master.sites)
             .then(function success(srv_resp){
-                $scope.sites    = srv_resp.data[0];
-                $scope.events   = angular.copy($scope.sites);
-                $scope.pag_inf  = srv_resp.data[1];
+                $scope.master   = srv_resp.data;
+                $scope.events   = angular.copy($scope.master);
             }, function failed(srv_resp) {
-                $scope.sites    = [{}];
+                $scope.master    = [{}];
             }
         );
     }
@@ -50,33 +44,31 @@ angular.module("vavaGaming").controller('noticeCtrl', function($scope, $http) {
         
         $http.post("notice/api/delete-events", $scope.events)
             .then(function success(srv_resp){
-                $scope.sites    = srv_resp.data[0];
-                $scope.events   = angular.copy($scope.sites);
-                $scope.pag_inf  = srv_resp.data[1];
+                $scope.master   = srv_resp.data;
+                $scope.events   = angular.copy($scope.master.sites);
             }, function failed(srv_resp) {
-                $scope.sites    = [{}];
+                $scope.master    = [{}];
             }
         );
     }
     $scope.saveNotice =function(){
     $http.post("notice/api/add-notice", $scope.new_notice)
     .then(function success(srv_resp){
-            $scope.sites    = srv_resp.data[0];
-            $scope.events   = angular.copy($scope.sites);
-            $scope.pag_inf  = srv_resp.data[1];
+            $scope.master    = srv_resp.data;
+            $scope.events   = angular.copy($scope.master);
         }, function failed(srv_resp) {
-            $scope.sites    = [{}];
+            $scope.master    = [{}];
         }
         );
     }
     $scope.saveEvent =function(){
     $http.post("notice/api/add-event", $scope.new_event)
     .then(function success(srv_resp){
-            $scope.sites    = srv_resp.data[0];
-            $scope.events   = angular.copy($scope.sites);
-            $scope.pag_inf  = srv_resp.data[1];
+            $scope.master    = srv_resp.data;
+            $scope.events   = angular.copy($scope.master.sites);
         }, function failed(srv_resp) {
-            $scope.sites    = [{}];
+            $scope.master    = [{}];
+            
         }
         );
     }
